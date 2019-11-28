90ae47d35c0e37795fbd5146c4a14a4bb42bf9b9
hchangjae <hcj@payw.org>
2019-11-28 22:26:51 +0900

    fix: nuxt-dev infinite reload bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,7 @@ export default {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: ['~plugins/vue-infinite-scroll'],
   /*
   ** Nuxt.js dev-modules
   */
@@ -45,9 +44,10 @@ export default {
   ** Build configuration
   */
   build: {
-    /*
-    ** You can extend webpack config here
-    */
+    filenames: {
+      app: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js',
+      chunk: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js'
+    },
     extend(config) {
       const svgRule = config.module.rules.find(rule =>
         rule.test.test('.svg')
